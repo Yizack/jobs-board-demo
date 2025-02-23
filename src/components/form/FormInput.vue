3145ef919d1d9cf72ea4ea119dffc2979e350e4b
Yizack Rangel <yizackr@gmail.com>
2025-02-22 21:14:17 -0500

    chore: add autocomplete

--- src/components/form/FormInput.vue
+++ src/components/form/FormInput.vue
@@ -8,6 +8,7 @@ defineProps<{
   icon?: string;
   placeholder?: string;
   emphasis?: boolean;
+  autocomplete?: string;
 }>();
 
 const model = defineModel<string>();
@@ -16,7 +17,7 @@ const model = defineModel<string>();
 <template>
   <div :class="{ 'form-input-icon': icon , 'form-input-floating': floating }">
     <Icon v-if="icon" :icon="icon" class="input-icon h-5 w-5" />
-    <input :id="id" v-model="model" :type="type || 'text'" class="from-input" :class="{ 'peer': floating, 'form-input-emphasis': emphasis }" :placeholder="!floating ? placeholder : ''">
+    <input :id="id" v-model="model" :type="type || 'text'" class="from-input" :class="{ 'peer': floating, 'form-input-emphasis': emphasis }" :placeholder="!floating ? placeholder : ''" :autocomplete="autocomplete">
     <label v-if="floating" :for="id" :class="{ 'pl-8' : icon, 'form-label-emphasis': emphasis }">
       {{ placeholder }}
     </label>
