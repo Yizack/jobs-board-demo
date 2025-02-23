7f54b8cb24e7543e4cd8d37a94204e481cc84d06
Yizack Rangel <yizackr@gmail.com>
2025-02-23 17:45:23 -0500

    feat: add footer

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { SITE } from "~/utils/site";
+</script>
+
+<template>
+  <footer class="bg-body-secondary">
+    <div class="container mx-auto py-8 text-center">
+      <p class="text-sm">{{ SITE.name }}</p>
+    </div>
+  </footer>
+</template>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import FooterSection from "~/components/FooterSection.vue";
 import NavBar from "~/components/NavBar.vue";
 </script>
 
 <template>
   <NavBar />
   <slot />
+  <FooterSection />
 </template>
