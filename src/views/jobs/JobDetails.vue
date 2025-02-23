87f62a9b8561bd74da48a50f37a3b8a5045dbf1e
Yizack Rangel <yizackr@gmail.com>
2025-02-23 05:13:19 -0500

    chore: start job detail

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+import { useJobsStore } from "~/stores/jobs";
+import { createError } from "~/utils/helpers";
+import HeaderSection from "~/components/HeaderSection.vue";
+
+const { params } = useRoute();
+
+const jobsStore = useJobsStore();
+const job = jobsStore.data.find((job) => job.id === Number(params.id));
+
+if (!job) {
+  throw createError({ message: "Job not found", statusCode: 404 });
+}
 </script>
 
 <template>
+  <HeaderSection :title="job.title" description="Search from our list of available jobs">
+    <router-link to="/jobs" class="font-semibold hover:underline">Back to jobs</router-link>
+  </HeaderSection>
   <main>
-    <span>Hello World</span>
+    {{ job }}
   </main>
 </template>
