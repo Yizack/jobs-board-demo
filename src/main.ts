bc66e4e6497f8651bae8fab13908107bef9e6efe
Yizack Rangel <yizackr@gmail.com>
2025-02-26 20:54:02 -0500

    refactor: css folder for styles

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { MotionPlugin } from "@vueuse/motion";
 import App from "~/App.vue";
 import router from "~/router";
 import { useMotionBinds } from "~/utils/motion";
-import "~/assets/main.css";
+import "~/assets/css/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
